5c5d91351aebd31726a8dc43b24dd90f98f05400
Christian Perez <christian.f.perez@gmail.com>
2017-07-11 10:33:45 -0700

    Fix create_sets

--- dl-tutorial-docker/create_sets.py
+++ dl-tutorial-docker/create_sets.py
@@ -7,11 +7,7 @@ import os
 from urllib.request import urlretrieve
 import zipfile
 
-FILE = 'cats-dogs.zip'
-if not os.path.exists(FILE):
-    print('Downloading data...')
-    urlretrieve('https://download.microsoft.com/download/3/E/1/3E1C3F21-ECDB-4869-8368-6DEBA77B919F/kagglecatsanddogs_3367a.zip',
-                FILE)
+FILE = sys.argv[1]
 
 if not os.path.exists('data'):
     zipf = zipfile.ZipFile(FILE, 'r')
